4e35942d0d8cec1cbd38f4dcb4ce95a81b503d41
Facundo <facu1910@gmail.com>
2019-07-09 00:55:02 -0300

    Create event detail page

---
+++ lib/ui/event_detail_page.dart
@@ -0,0 +1,93 @@
+import 'package:come_in/models/event.dart';
+import 'package:flutter/material.dart';
+
+class EventDetailPage extends StatefulWidget {
+  final ComeInEvent event;
+  EventDetailPage({Key key, this.event}) : super(key: key);
+  @override
+  _EventDetailPageState createState() => _EventDetailPageState();
+}
+
+class _EventDetailPageState extends State<EventDetailPage> {
+  @override
+  Widget _buildEventDetail() {
+    return ListView(
+      children: [
+        Card(
+          margin: EdgeInsets.all(10),
+          elevation: 6,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+          child: Padding(
+            padding: EdgeInsets.all(10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Event Name',
+                      style: TextStyle(color: Colors.grey, fontSize: 15),
+                    ),
+                    Text(
+                      widget.event.title,
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ],
+                ),
+                Divider(),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Event Description',
+                      style: TextStyle(color: Colors.grey, fontSize: 15),
+                    ),
+                    Text(
+                      widget.event.description,
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ],
+                ),
+                Divider(),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Event Location',
+                      style: TextStyle(color: Colors.grey, fontSize: 15),
+                    ),
+                    Text(
+                      widget.event.location,
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.event.title),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.more_vert),
+            onPressed: () {},
+          )
+        ],
+      ),
+      body: Container(child: _buildEventDetail()),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.edit),
+      ),
+    );
+  }
+}
--- lib/ui/events_page.dart
+++ lib/ui/events_page.dart
@@ -3,6 +3,8 @@ import 'package:come_in/bloc/comein_bloc.dart';
 import 'package:come_in/models/event.dart';
 import 'package:come_in/providers/comein_provider.dart';
 
+import 'event_detail_page.dart';
+
 class EventPage extends StatefulWidget {
   @override
   _EventPageState createState() => _EventPageState();
@@ -14,35 +16,46 @@ Widget _buildEventList(List<ComeInEvent> list) {
     itemBuilder: (BuildContext context, int index) {
       ComeInEvent events = list[index];
       return Container(
-        child: Card(
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
-          margin: EdgeInsets.all(15),
-          child: Padding(
-            padding: EdgeInsets.all(15.0),
-            child: Wrap(
-              children: [
-                Text(
-                  '${events.title}',
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+        child: GestureDetector(
+          onTap: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => EventDetailPage(
+                  event: list[index],
                 ),
-                Divider(),
-                Text('${events.description}'),
-                Divider(),
-                Row(
-                  children: [
-                    Icon(Icons.location_on),
-                    Text('${events.location}'),
-                  ],
-                ),
-                Divider(),
-                Row(
-                  children: [
-                    Icon(Icons.calendar_today),
-                    Text('Event date will appear here'),
-                  ],
-                )
-              ],
+              ),
+            );
+          },
+          child: Card(
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+            margin: EdgeInsets.all(15),
+            child: Padding(
+              padding: EdgeInsets.all(15.0),
+              child: Wrap(
+                children: [
+                  Text(
+                    '${events.title}',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  Divider(),
+                  Text('${events.description}'),
+                  Divider(),
+                  Row(
+                    children: [
+                      Icon(Icons.location_on),
+                      Text('${events.location}'),
+                    ],
+                  ),
+                  Divider(),
+                  Row(
+                    children: [
+                      Icon(Icons.calendar_today),
+                      Text('Event date will appear here'),
+                    ],
+                  )
+                ],
+              ),
             ),
           ),
         ),
@@ -65,15 +78,14 @@ class _EventPageState extends State<EventPage> {
           onPressed: () {},
         ),
       ),
-      body: 
-        StreamBuilder(
-          stream: comeInBloc.events,
-          builder: (BuildContext context, AsyncSnapshot snapshot) {
-            return snapshot.hasData
-                ? _buildEventList(snapshot.data)
-                : Center(child: CircularProgressIndicator());
-          },
-        ),
+      body: StreamBuilder(
+        stream: comeInBloc.events,
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          return snapshot.hasData
+              ? _buildEventList(snapshot.data)
+              : Center(child: CircularProgressIndicator());
+        },
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.of(context).pushNamed('/createevent');
